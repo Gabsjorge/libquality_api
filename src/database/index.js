ed4f5a9f9e0e5a7174197d0ca6b3f289003a89b4
Gabsjorge <gabriela.fejorge@gmail.com>
2021-07-27 00:01:29 -0300

    chore: add models to sequelize connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,14 @@
 const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
 
+const User = require("../models/User");
+const Repository = require("../models/Repository");
+const Search = require("../models/Search");
+
 const connection = new Sequelize(dbConfig);
+
+User.init(connection);
+Repository.init(connection);
+Search.init(connection);
 
 module.exports = connection;
