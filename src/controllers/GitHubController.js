adc81de30462161cc20ef0d48603af74f79a18ed
Gabsjorge <gabriela.fejorge@gmail.com>
2021-07-30 21:24:33 -0300

    feat: add routes and controller to GitHubAPI usage

---
+++ src/controllers/GitHubController.js
@@ -0,0 +1,89 @@
+const axios = require("axios").default;
+
+const baseURL = 'https://api.github.com/repos/';
+const auth = {
+    username: 'Gabsjorge',
+    password: '',
+};
+
+module.exports = {
+    async getIssuesCount(req, res) {
+        console.log(req.params);
+        const { owner, repo } = req.params;
+
+        const response = await axios({
+            method: 'get',
+            url: baseURL + `${owner}/${repo}/repos`,
+            // auth,
+        });
+
+        return res.json();
+    },
+
+    async getIssuesOpenTime(req, res) {
+        console.log(req.params);
+        const { owner, repo } = req.params;
+
+        const totalIssues = this.getIssuesCount(req, res);
+        
+        // Sets all auxiliary functions
+        const convertTime = () => {
+
+        };
+
+        const getTimeDiff = (time) => {
+            const now = Date.now();
+
+            return now - convertTime(time);
+        };
+
+        const avgIssueTime = () => {
+
+        };
+
+        // std stands for Standard Deviation Time
+        const stdIssueTime = () => {
+
+        };
+
+        /* 
+        * Calculates total number of pages necessary for pagination in GitHub request for issues
+        * Needs to be rounded up always (Math.ceil), so it doesn't leave issues behind
+        * Using 100 pages per page to get as many issues per page as possible
+        */
+        const totalPages = Math.ceil(totalIssues / 100);
+
+        /* 
+        * Because Axios is Promises-based to make requests, I'll use axios.all() to execute all
+        * GitHub requests needed per page 
+        */ 
+        const pageRequest = (page) => {
+            return axios({
+                method: 'get',
+                url: baseURL + `${owner}/${repo}/issues?state=open&per_page=100/${page}`
+            })
+        };
+
+        const executeAllRequests = () => {
+            const totalPromises = [];
+            
+            for (let page in totalPages) {
+                totalPromises.push(pageRequest(page));
+            }
+
+            return totalPromises;
+        }
+
+        
+        try {
+            axios.all(executeAllRequests);
+        } catch (error) {
+            throw error;
+        }
+
+        return res.json({
+            avgTime: avgIssueTime(),
+            stdTime: stdIssueTime()
+        });
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const UserController = require("./controllers/UserController");
+const RepositoryController = require("./controllers/RepositoryController");
+
+const GitHubController = require("./controllers/GitHubController");
 
 const routes = express.Router();
 
+// Database routes
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+// API routes
+routes.get('/:owner/:repo/issues/count', GitHubController.getIssuesCount);
+routes.get('/:owner/:repo/issues/time', GitHubController.getIssuesOpenTime);
 
 module.exports = routes;
