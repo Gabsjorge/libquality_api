6ca56bec7b98a9b3a59ae01503a5863d07ec0e0f
Gabsjorge <gabriela.fejorge@gmail.com>
2021-07-30 21:23:59 -0300

    feat: add create function to Repository Controller

--- src/controllers/RepositoryController.js
+++ src/controllers/RepositoryController.js
@@ -2,6 +2,10 @@ const Repository = require("../models/Repository");
 
 module.exports = {
     async store(req, res) {
+        const { name, owner, issues_count } = req.body;
 
+        const repo = Repository.create({ name, owner, issues_count });
+
+        return res.json(repo);
     }
 };
